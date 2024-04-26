8577ca8e4348d7838bad4c2a48975144e5015c9b
DonaldVargas <donald.vargasgarcia@ucr.ac.cr>
2024-04-26 11:28:44 -0600

    funcion 5 agregada

--- funciones/9-encabezados.c
+++ funciones/9-encabezados.c
@@ -6,12 +6,15 @@ int main() {
     int v;
 
     // Llama a las 3 funciones
+
     funcion1();
     funcion2(5, 2.67);
     v = funcion3(10);
-
+    v1 = funcion5(10);
     // Imprime el valor que retornó funcion3()
     printf("v = %d\n", v);
+    // Imprime el valor que retornó funcion5()
+    printf("v1 = %d\n", v1);
 
     return 0;
 
@@ -33,5 +36,12 @@ void funcion2(int a, float b) {
 int funcion3(int x) {
     printf("En funcion3()...\n");
     int y = x + 2;
+    return y;
+}
+// funcion5(): Imprime un mensaje. Retorna el valor que recibió como argumento
+// incrementado en 2
+int funcion5(int x) {
+    printf("En funcion5()...\n");
+    int y = x + 3;
     return y;
 }
